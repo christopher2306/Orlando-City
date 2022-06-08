d3b91199f4d70b8d95d23a829a9145ad37492ba5
Christopher Antony <christophercarreira2306@gmail.com>
2022-06-08 16:38:46 -0300

    Versão final - Tornando Responsivo

--- js/efeitos.js
+++ js/efeitos.js
@@ -1,31 +1,74 @@
 $(document).ready(function(){
 
-		$("#logotipo").on("mouseover", function(){
+	$("#logotipo").on("mouseover",function(){
+		
+		$("#banner h1").addClass("efeito");
 
-			$("#logotipo").addClass("efeito");
+	}).on("mouseout", function(){
 
-		}).on("mouseout", function(){
-
-			$("#logotipo").removeClass("efeito");
-
-		});
-
-		$(".thumbnails").owlCarousel({
-			loop:true,
-			margin: 10,
-			//nav:true,
-			//navText: ["Anterior","Próximo"],
-			responsive: {
-				0:{
-					items: 1
-				},
-				480:{
-					items: 3
-				},
-				1000:{
-					items: 4
-				},
-			}
-		});		
+		$("#banner h1").removeClass("efeito");
 
 	});
+
+	$("#input-search").on("focus", function(){
+
+		$("li.search").addClass("ativo");
+
+	}).on("blur", function(){
+
+		$("li.search").removeClass("ativo");				
+
+	});
+
+	$(".thumbnails").owlCarousel({
+ 
+      //autoPlay: 3000,
+      items : 4
+ 
+  	});
+
+  	var owl = $(".thumbnails").data('owlCarousel');
+
+  	$('#btn-news-prev').on("click", function(){
+
+  		owl.prev();
+
+  	});
+
+  	$('#btn-news-next').on("click", function(){
+
+  		owl.next();
+
+  	});
+
+  	$("#page-up").on("click", function(event){
+
+  		$("body").animate({
+  			scrollTop:0
+  		}, 1000);
+
+  		event.preventDefault();
+
+  	});
+
+    $("#btn-bars").on("click", function(){
+
+      $("header").toggleClass("open-menu");
+
+    });
+
+    $("#menu-mobile-mask, .btn-close").on("click", function(){
+
+      $("header").removeClass("open-menu");
+
+
+    });
+
+    $("#btn-search").on("click", function(){
+
+      $("header").toggleClass("open-search");
+      $("#input-search-mobile").focus();
+
+    });
+
+});
